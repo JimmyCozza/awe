93400d8935a9dd21c750a4e04c3cf3c585a3baf9
Jimmy Cozza <moatcozza@gmail.com>
2022-10-16 14:56:56 -0600

    Dump notifications to a dumb file

--- ui/notifications/init.lua
+++ ui/notifications/init.lua
@@ -1,12 +1,17 @@
 local naughty = require "naughty"
+local awful = require "awful"
+local noscope = require "noscope"
 local gears = require "gears"
 local wibox = require "wibox"
 
 naughty.connect_signal("request::display", function(n)
+awful.spawn.easy_async_with_shell("echo " .. " - " .. n.app_name .. " - " .. n.urgency .. " - " .. n.title .. " - " .. n.text .. " >> ~/awe/test.txt")
   naughty.layout.box {
     notification = n,
     ontop = true,
     position = "top_right",
+    border_width = 3,
+    border_color = "#458588",
     shape = gears.shape.rectangle,
     widget_template = {
       widget = wibox.container.constraint,
@@ -27,12 +32,14 @@ naughty.connect_signal("request::display", function(n)
               widget = wibox.container.constraint,
               width = dpi(48),
               height = dpi(48),
+              --border_color = "#fb4934",
               strategy = "max",
               naughty.widget.icon,
             },
             {
               layout = wibox.layout.fixed.vertical,
               spacing = dpi(8),
+              --border_color = "#fb4934",
               naughty.widget.title,
               naughty.widget.message {
                 spacing = dpi(8),
